b6bb4f486cf2299c6b7ebaded8eeea203f215037
Vander-Reis <vanderreis2017@outlook.com>
2023-01-23 18:06:00 -0300

    alteração da porta e adição do .env

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
 import { appRoutes } from './routes';
+import * as dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = Fastify();
 
 app.register(cors);
 
 app.register(appRoutes);
+const ports = Number(process.env.PORTS)
 
 
 app.listen({
-  port: 5555,
+  port: ports || 3333,
   // host: '192.168.18.65',
 }).then(() => {
-  console.log('Http Server running')
+  console.log('Http Server running on', ports)
 });
