69ea086942583d99eb5f161c55d2c2fe00d62ec5
Vander-Reis <vanderreis2017@outlook.com>
2023-01-23 18:35:36 -0300

    express

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-import Fastify from "fastify";
-import cors from "@fastify/cors"
+import express  from "express";
+import cors from "cors"
 import { appRoutes } from './routes';
 import * as dotenv from 'dotenv'
 
 dotenv.config();
 
-const app = Fastify();
+const app = express();
 
-app.register(cors);
+app.use(cors());
 
-app.register(appRoutes);
-const ports = Number(process.env.PORTS)
+app.use(appRoutes);
+
+const PORT = process.env.PORTS || 3333;
 
 
-app.listen({
-  port: ports || 3333,
-  // host: '192.168.18.65',
-}).then(() => {
-  console.log('Http Server running on', ports)
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
